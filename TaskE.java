4b50787b0bcbc2c2daf499bb1bd6e7c1e5ca4040
Isaiah <iazelayandia@cpp.edu>
2022-01-31 20:22:29 -0800

    This is my first commit

---
+++ TaskE.java
@@ -0,0 +1,14 @@
+public class TaskE
+{
+    public static void main(String [] args)
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 5; x < y; y--)
+            {
+                System.out.print(":) ");
+            }
+            System.out.println("");
+        }
+    }
+}
